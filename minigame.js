b507c7bbf84ae1f8dbedd920cf63218350d5914c
Max de Bono <maxdebono@proton.me>
2025-07-29 16:48:03 +0100

    Minigame type selection

---
+++ minigame.js
@@ -0,0 +1,60 @@
+const minigameButtons = {
+    elements: document.querySelectorAll(".minigame-button"),
+    description: [
+        "all vs all",
+        "1 vs 5",
+        "two teams",
+        "three teams"
+    ],
+    currentIndex: 0,
+    currentType: document.querySelector("#minigame-type"),
+    rewards: document.querySelectorAll(".reward")
+}
+
+const spinner = {
+    elements: document.querySelectorAll("#spinner"),
+    type: "",
+    duration: 1,
+    minigames: getMinigames()
+}
+
+
+//----
+minigameButtons.elements.forEach(addClickCallback);
+
+function addClickCallback(button,index) {
+    button.addEventListener("pointerdown",event => handlePointerDown(event,minigameButtons,index),passive=true);
+}
+
+function handlePointerDown(event,minigameButtons,index) {
+    minigameButtons.currentType.innerHTML = minigameButtons.description[index];
+    minigameButtons.currentIndex = index; 
+    setReward(minigameButtons.rewards,index);
+    spinWheel(spinner,index)
+}
+
+function setReward(rewards,index) {
+    rewards.forEach(reward => reward.style.visibility = "visible");
+    
+    let numRewards;
+    switch (index){
+        case 0: numRewards = 5;break;
+        case 1: numRewards = 1;break;
+        case 2: numRewards = 1;break;
+        case 3: numRewards = 2;break;
+    }
+
+    const hiddenRewards = Array.from(rewards).slice(numRewards);
+    hiddenRewards.forEach(reward => reward.style.visibility = "hidden");
+}
+//---
+function getMinigames() {
+    fetch('./data/all-vs-all-minigames.txt')
+        .then(response => response.text())
+        .then(text => console.log(text));
+}
+
+
+function spinWheel(spinner,index) {
+
+}
