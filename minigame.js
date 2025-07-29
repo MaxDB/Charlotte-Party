30e4d43e9a753b129f7f0a9ddae2c7a81c44d9e2
Max de Bono <maxdebono@proton.me>
2025-07-29 18:07:37 +0100

    Added spinner functionality - minimum working version

--- minigame.js
+++ minigame.js
@@ -12,13 +12,15 @@ const minigameButtons = {
 }
 
 const spinner = {
-    elements: document.querySelectorAll("#spinner"),
-    type: "",
+    element: document.querySelector("#spin-animation"),
     duration: 1,
-    minigames: getMinigames()
+    minigames: getMinigames(),
+    gameName: "",
+    gameDescription: "",
+    gameNameElement: document.querySelector("#game-name"),
+    gameDescriptionElement: document.querySelector("#game-description"),
+    isSpinning: false
 }
-
-
 //----
 minigameButtons.elements.forEach(addClickCallback);
 
@@ -27,6 +29,7 @@ function addClickCallback(button,index) {
 }
 
 function handlePointerDown(event,minigameButtons,index) {
+    if (spinner.isSpinning) {return};
     minigameButtons.currentType.innerHTML = minigameButtons.description[index];
     minigameButtons.currentIndex = index; 
     setReward(minigameButtons.rewards,index);
@@ -48,19 +51,71 @@ function setReward(rewards,index) {
     hiddenRewards.forEach(reward => reward.style.visibility = "hidden");
 }
 //---
+spinner.element.addEventListener("animationend", (event) => spinnerFinish(event,spinner))
+
 function getMinigames() {
-    let minigameData;
-    fetch('./data/all-vs-all-minigames.txt')
-        .then(response => response.text())
-        .then(text => minigameData = text.split(","))
-        .catch(error => console.error("Error fetching file:", error));
-    console.log(minigameData)
-    if (minigameData === undefined){
-        
-    }
+    const minigameNames = [
+        "all-vs-all",
+        "one-vs-five",
+        "two-teams",
+        "three-teams"
+    ]
+
+    return minigameNames.map(loadMinigames)
 }
 
+function loadMinigames(name) {
+    const filename = "./data/" + name + "-minigames.txt";
+    // fetch(filename)
+       // .then(response => response.text())
+        //.then(text => console.log(text))
+        //.catch(error => console.error("Error fetching file:", error));
+    
+        let text = ` 
+            game-1, ${name} -description-1;
+            game-2, ${name} -description-2;
+            game-3, ${name} -description-3;
+            game-4, ${name} -description-4;
+            game-5, ${name} -description-5;
+            game-6, ${name} -description-6;
+            game-7, ${name} -description-7;
+            game-8, ${name} -description-8;
+            game-9, ${name} -description-9;
+            game-10, ${name} -description-10
+        `
+        minigameData = text.replace(/(\r\n|\r|\n| )/g,"").split(";")
+            .map(game => game.split(","));
+    
+    return minigameData
+}
 
 function spinWheel(spinner,index) {
+    const minigames = spinner.minigames[index];
+    const gameIndex = Math.floor(Math.random() * minigames.length);
+    const [gameName,gameDescription] = minigames[gameIndex];
+
+    spinner.gameName = gameName;
+    spinner.gameDescription = gameDescription;
+
+    startSpinning(spinner)
+}
+
+function startSpinning(spinner) {
+    spinner.isSpinning = true;
+    spinner.element.style.opacity = 1;
+    spinner.element.style.animationPlayState = "running";
+
+}
+
+function spinnerFinish(event,spinner){
+    spinner.gameNameElement.innerHTML = spinner.gameName;
+    spinner.gameDescriptionElement.innerHTML = spinner.gameDescription;
+     spinner.element.style.animationPlayState = "paused";
+     spinner.element.style.opacity = 0;
+     spinner.isSpinning = false;
+    
+    spinner.element.style.animation = 'none'
+    void spinner.element.offsetWidth;
+    spinner.element.style.animation = null; 
 
 }
